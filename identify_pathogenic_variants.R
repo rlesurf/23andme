2a3d9043fbf5d4daf52d6ee8b5d1d956aeffc688
Robert Lesurf <robert.lesurf@gmail.com>
2017-11-07 19:51:43 -0500

    cleaned up matrix output

--- identify_pathogenic_variants.R
+++ identify_pathogenic_variants.R
@@ -11,7 +11,7 @@ clinvar = clinvar[which(clinvar$Assembly == "GRCh37"), ]
 
 # Get the common variants, but only keep ones where I have an alternate allele
 common_rsid = intersect(as.character(variants$rsid), paste0("rs", clinvar$RS...dbSNP.))
-pvariants = NULL
+pvariants = matrix(nrow = 0, ncol = ncol(variants)+ncol(clinvar), dimnames=list(NULL, c(colnames(variants), colnames(clinvar))))
 for(i in common_rsid) {
 	ttam_index = which(variants$rsid == i)
 	ttam_alleles = as.character(variants$genotype[ttam_index])
@@ -19,8 +19,10 @@ for(i in common_rsid) {
 		ttam_alleles = c(substr(ttam_alleles, 1, 1), substr(ttam_alleles, 2, 2))
 	clinvar_index = which(clinvar$RS...dbSNP. == substr(i, 3, nchar(i)))
 	clinvar_index_specific = clinvar_index[which(as.character(clinvar$AlternateAllele[clinvar_index]) %in% ttam_alleles)]
-	if(length(clinvar_index_specific) > 0)
-		pvariants = rbind(pvariants, as.character(variants[ttam_index, ]), as.character(clinvar[clinvar_index_specific, ]))
+	if(length(clinvar_index_specific) > 1)
+		stop(paste("Dang Rob, you've got multiple different pathologic variants at", i))
+	if(length(clinvar_index_specific) == 1)
+		pvariants = rbind(pvariants, c(as.character(as.matrix(variants[ttam_index, ])), as.character(as.matrix(clinvar[clinvar_index_specific, ]))))
 }
 
 # Save results
